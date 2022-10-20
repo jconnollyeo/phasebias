007e0eb0446db704c8f86cf2ac650b85f7b3bcc8
Jacob Connolly <py15jmc@leeds.ac.uk>
2022-10-20 11:30:58 +0100

    Split into segments and plotting functionality

--- loopClosure_post.py
+++ loopClosure_post.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-from datetime import date, datetime
-
+from datetime import datetime
+from utils import multilook
 # Import the data
 fp = '/home/jacob/phase_bias/12_6_6/data/*.npy'
 
@@ -31,21 +31,58 @@ def importData(fns):
     return out
 
 def splitGrids(data, size=100):
-
+    """
+    Splits an array by the size variable and computes the mean of each segment. 
+    """
     
+    i = list(np.arange(data.shape[0])[::size])
+    i.append(None)
 
-    return ""
+    j = list(np.arange(data.shape[1])[::size])
+    j.append(None)
+
+    out = np.empty(data.shape, dtype=type(data[0, 0]))
+
+    for i_start, i_end in zip(i[:-1], i[1:]):
+        for j_start, j_end in zip(j[:-1], j[1:]):
+            out[i_start:i_end, j_start:j_end] = np.mean(data[i_start:i_end, j_start:j_end])
+
+    return out
+
+def splitTS(data, size=100):
+    """
+    Splits each 2d array by the size variable and computes the mean of each segment and then 
+    for each segment return the timeseries. 
+    """
+    out = np.empty(data.shape, dtype=type(data.flatten()[0]))
+
+    for i, d in enumerate(data):
+        segmented = splitGrids(d, size=size)
+        out[i] = segmented
+
+    return out[:, ::size, ::size]
 
 def main():
+    size=50
+    
     dates_primary, dates_secondary = extractDates(data_fns)
 
     data = importData(data_fns)
+    data_complex = np.exp(1j*data)
+
     print (data.shape)
 
     # Plot the timeseries of mean closure phase for the whole image
-    plt.figure()
-    plt.plot(dates_primary, np.array([np.mean(d) for d in data]))
-    plt.title("Mean closure phase for entire image")
+    fig, ax_TS = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+    ax_TS[0].plot(dates_primary, np.array([np.mean(d) for d in data]))
+    ax_TS[0].set_title("Mean closure phase for entire image")
+
+    mdata = splitTS(data_complex, size=size)
+    mdata_ = np.angle(mdata.reshape((mdata.shape[0], np.prod(mdata.shape[1:]))))
+
+    for m in mdata_.T:
+        ax_TS[1].plot(dates_primary, m)
+    ax_TS[1].set_title("Mean closure phase of each segment")
 
     # Plot the season boundaries
     plt.axvline(x=datetime.strptime("20210201", "%Y%m%d"), color="red")
@@ -53,6 +90,16 @@ def main():
     plt.axvline(x=datetime.strptime("20210801", "%Y%m%d"), color="red")
     plt.axvline(x=datetime.strptime("20211101", "%Y%m%d"), color="red")
 
+    fig, ax = plt.subplots(nrows=1, ncols=4, figsize=(16, 5))
+    ix = 10
+    ax[0].matshow(multilook(np.load("mean_amp.npy"), 3, 12), cmap="binary_r", vmax=3, vmin=0)
+    p1 = ax[1].matshow(data[ix], cmap='RdYlBu', vmin=-np.pi, vmax=np.pi)
+    p2 = ax[2].matshow(np.angle(splitGrids(data_complex[ix], size=size)), cmap='RdYlBu', vmin=-0.5, vmax=0.5)
+    n = ax[3].hist(data[ix].flatten(), bins=np.linspace(-np.pi, np.pi, 50))[0]
+    ax[3].text(-0.5, np.max(n)*0.7, f"$\mu$ = {np.mean(data[ix]):.2f}", horizontalalignment='right')
+
+    plt.colorbar(p1, ax=ax[1], orientation='horizontal')
+    plt.colorbar(p2, ax=ax[2], orientation='horizontal')
 
     plt.show()
 
