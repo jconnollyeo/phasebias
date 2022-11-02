ebfa37dd177e4641a30844169762d8e644ae5e94
Jacob Connolly <py15jmc@leeds.ac.uk>
2022-11-02 14:56:48 +0000

    Creates single image

--- Fig3_Yasser.py
+++ Fig3_Yasser.py
@@ -5,6 +5,7 @@ import sys
 from datetime import datetime, timedelta
 import argparse
 import h5py as h5
+from utils import multilook
 
 def parse_args():
     """
@@ -40,12 +41,17 @@ def parse_args():
                         type=bool,
                         dest="save",
                         help="Boolean to save (True) or not save (False).",
-                        default=1)
+                        default=0)
     parser.add_argument("-p",
                         type=bool,
                         dest="plot",
                         help="Boolean to show plot (True) or not show plot (False).",
                         default=1)
+    parser.add_argument("-a",
+                        type=str,
+                        dest='multilook',
+                        help="Multilook factor",
+                        default='3,12')
     args = parser.parse_args()
     return vars(args)
 
@@ -54,35 +60,42 @@ def check_chain(dates, length):
     # print ("End ", dates[-1])
     # print ((dates[-1]-dates[0]).days)
     d = dates[0]
-    ixs = []
+    ixs = [0]
 
     while not (d == dates[-1]):
         if d+timedelta(days=length) in dates:
-            print (np.where(d+timedelta(days=length) == dates))
-            ixs.append(int(np.where(d+timedelta(days=length) == dates)[0]))
+            # print (np.where((np.array(d+timedelta(days=length)) == dates))[0][0])
+            # print (np.where(((d+timedelta(days=length)) == dates)))
+            ixs.append(int(np.where((np.array(d+timedelta(days=length)) == dates))[0][0]))
             d = d+timedelta(days=length)
         else:
             print (d)
             return False, []
-
+    print (ixs)
     return True, ixs
 
 def main():
+    plt.rcParams['image.cmap'] = 'RdYlBu'
+
     args_dict = parse_args()
     wdir = str(args_dict["wdir"])
     start = int(args_dict["start"])
     length = int(args_dict["length"])
     m = int(args_dict["m"])
     n = int(args_dict["n"])
+    ml = np.array(args_dict["multilook"].split(','), dtype=int)
     
     fns = getFiles(wdir)
 
     save_bool = args_dict["save"]
     plot = args_dict["plot"]
 
-    diff = doLoops(fns, start, length, m, n)
-    plt.matshow(diff)
-    plt.colorbar()
+    diff = doLoops(fns, start, length, m, n, ml)
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 8))
+    p = ax[0].matshow(diff)
+    ax[1].hist(diff.flatten(), bins=np.linspace(-np.pi, np.pi, 30))
+    plt.colorbar(p, ax=ax)
+
     if save_bool:
         plt.savefig("test.jpg")
     else:
@@ -92,8 +105,7 @@ def main():
         plt.show()
 
 
-
-def doLoops(fns, start, length, m, n):
+def doLoops(fns, start, length, m, n, ml):
     # === Description ===
 
     # The 60 days span the entire 360 days, then the shorter 
@@ -115,32 +127,32 @@ def doLoops(fns, start, length, m, n):
     n_chain_check, n_ixs = check_chain(dates[start:end+1], length=n)
 
     m_chain_check, m_ixs = check_chain(dates[start:end+1], length=m)
-    
-    print (n, n_chain_check)
-    print (m, m_chain_check)
 
     # Calculate the days
 
     # Create daisy chain of n-day ifgs (60 days)
     n_days = np.arange(0, length+1, n) # [0, 60, 120, 180, ..., 360]  
-    shape = np.asarray(h5.File(fns[0])['Phase']).shape # Fetch the shape of the data
+    shape = multilook(np.asarray(h5.File(fns[0])['Phase']), ml[0], ml[1]).shape # Fetch the shape of the data
     n_ifgs_summed = np.zeros(shape) 
-
-    for p_fn, s_fn in zip(n_ixs[:-1], n_ixs[:1]): 
+    print (dates[n_ixs[0]], dates[n_ixs[-1]])
+    for p_fn, s_fn in zip(n_ixs[:-1], n_ixs[1:]): 
+        print (f"n, {p_fn = }, {s_fn = }", end='\r')
         p = np.asarray(h5.File(fns[p_fn])['Phase']) 
         s = np.asarray(h5.File(fns[s_fn])['Phase']) 
 
-        n_ifgs_summed += np.angle(np.exp(1j*(s-p)))
+        n_ifgs_summed += np.angle(multilook(np.exp(1j*(s-p)), ml[0], ml[1]))
 
     # Create a daisy chain of m-day ifgs (6, 12, 18, etc. days)
     m_days = np.arange(0, length+1, m)
     m_ifgs_summed = np.zeros(shape)
+    print (dates[m_ixs[0]], dates[m_ixs[-1]])
     
-    for p_fn, s_fn in zip(m_ixs[:-1], m_ixs[:1]):
+    for p_fn, s_fn in zip(m_ixs[:-1], m_ixs[1:]):
+        print (f"m, {p_fn = }, {s_fn = }", end='\r')
         p = np.asarray(h5.File(fns[p_fn])['Phase'])
         s = np.asarray(h5.File(fns[s_fn])['Phase'])
 
-        m_ifgs_summed += np.angle(np.exp(1j*(s-p)))
+        m_ifgs_summed += np.angle(multilook(np.exp(1j*(s-p)), ml[0], ml[1]))
     
     # Find the difference between them
     diff = np.angle(np.exp(1j*(n_ifgs_summed - m_ifgs_summed)))
